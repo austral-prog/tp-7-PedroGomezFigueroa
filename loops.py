0f14f21f8b3af99c6dfac15aaa1dfbe8efc02ca1
PedroGomezFigueroa <162721991+PedroGomezFigueroa@users.noreply.github.com>
2024-05-23 22:49:44 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,42 @@
 def index_of_by_index(word, list, index):
-    return -1
 
+    if(word in list):
+        if (list.index(word)>=index):
+            return list.index(word)
+        else:
+            if word in list[index:]:
+                return list[index:].index(word) + index
+            else:
+                return -1
+        
+    else:
+        return -1
 
 def index_of_empty(list):
-    return -1
 
+    if "" in list:
+        return list.index("")
+    else:
+        return -1
 
 def index_of(word, list):
-    return -1
-
+    if word in list:
+        return list.index(word)
+    else:
+        return -1
 
 def put(word, list):
+    for i, vacio in enumerate(list):
+        if vacio == "":
+            list[i] = word
+            return i 
     return -1
-
 
 def remove(word, list):
-    return -1
+    iti = 0
+    for i, num in enumerate(list):
+        if num == word:
+            list[i] = ""
+            iti +=1
+
+    return iti
